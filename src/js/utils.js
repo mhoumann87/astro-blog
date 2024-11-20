17ad5540cd062c11953edea89fc3e71c847f3665
Michael Houmann <mhoumann87@gmail.com>
2024-11-20 10:31:32 +0100

    Add function to filter posts and add to blog page

--- src/js/utils.js
+++ src/js/utils.js
@@ -14,3 +14,43 @@ export function formatDate(date) {
     timeZone: 'UTC',
   });
 }
+
+export function formatBlogPosts(
+  posts,
+  {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = undefined,
+  } = {}
+) {
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+    // filter out drafts if true
+    if (filterOutDrafts && draft) return acc;
+
+    // filter out future posts if true
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+    // add post to acc
+    acc.push(post);
+
+    return acc;
+  }, []);
+
+  // Sort by date or randomize
+  if (sortByDate) {
+    filteredPosts.sort(
+      (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+    );
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5);
+  }
+
+  // limit if number is passed
+  if (typeof limit === 'number') {
+    return filteredPosts.slice(0, limit);
+  }
+
+  return filteredPosts;
+}
